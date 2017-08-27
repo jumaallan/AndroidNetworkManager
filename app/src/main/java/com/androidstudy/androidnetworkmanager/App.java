419698575ce49f06af4891a94e43e17080a7d7b0
Juma Allan <jumadeveloper@gmail.com>
2017-08-27 10:49:15 +0300

    Updated

---
+++ app/src/main/java/com/androidstudy/androidnetworkmanager/App.java
@@ -0,0 +1,28 @@
+package com.androidstudy.androidnetworkmanager;
+
+import android.app.Application;
+
+/**
+ * Created by anonymous on 8/27/17.
+ */
+
+public class App extends Application {
+    // Gloabl declaration of variable to use in whole app
+
+    public static boolean activityVisible; // Variable that will check the
+    // current activity state
+
+    public static boolean isActivityVisible() {
+        return activityVisible; // return true or false
+    }
+
+    public static void activityResumed() {
+        activityVisible = true;// this will set true when activity resumed
+
+    }
+
+    public static void activityPaused() {
+        activityVisible = false;// this will set false when activity paused
+
+    }
+}
--- app/src/main/java/com/androidstudy/androidnetworkmanager/MainActivity.java
+++ app/src/main/java/com/androidstudy/androidnetworkmanager/MainActivity.java
@@ -1,13 +1,58 @@
 package com.androidstudy.androidnetworkmanager;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static TextView internetStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        internetStatus = (TextView) findViewById(R.id.internet_status);
+
+        // At activity startup we manually check the internet status and change
+        // the text status
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            changeTextStatus(true);
+        } else {
+            changeTextStatus(false);
+        }
+
+    }
+
+    // Method to change the text status
+    public void changeTextStatus(boolean isConnected) {
+
+        // Change status according to boolean value
+        if (isConnected) {
+            internetStatus.setText("Internet Connected.");
+            internetStatus.setTextColor(Color.parseColor("#00ff00"));
+        } else {
+            internetStatus.setText("Internet Disconnected.");
+            internetStatus.setTextColor(Color.parseColor("#ff0000"));
+        }
+    }
+
+    @Override
+    protected void onPause() {
+
+        super.onPause();
+        App.activityPaused();// On Pause notify the Application
+    }
+
+    @Override
+    protected void onResume() {
+
+        super.onResume();
+        App.activityResumed();// On Resume notify the Application
     }
 }
